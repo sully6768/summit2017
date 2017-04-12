bdeb7ffb726cea50b96ddde7d7ec01a50646a151
SES <scottes@redhat.com>
2017-04-12 14:39:10 -0400

    moved all beans to XML

--- summit2017-fuse-camel-springboot/src/main/java/com/redhat/jboss/fuse/Application.java
+++ summit2017-fuse-camel-springboot/src/main/java/com/redhat/jboss/fuse/Application.java
@@ -18,18 +18,20 @@ package com.redhat.jboss.fuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-// load regular Spring XML file from the classpath that contains the Camel XML DSL
-@ImportResource({"classpath:spring/camel-context.xml"})
-public class Application {
+// load regular Spring XML file from the classpath that contains the Camel XML
+// DSL
+@ImportResource({ "classpath:spring/camel-context.xml" })
+public class Application extends SpringBootServletInitializer {
 
-    /**
-     * A main method to start this application.
-     */
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	/**
+	 * A main method to start this application.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
